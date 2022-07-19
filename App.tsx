50920bcd13887dba86d7a2b32573724de1016c5f
mateohideg <mateo.hideg@hotmail.com>
2022-07-19 01:09:17 -0300

    I forgot to change this after debugging

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import Reports from './Reports';
 import Activities from './Activities';
 import Settings from './Settings';
 
-let domain = process.env.DOMAIN;
+const domain = process.env.DOMAIN;
 
 export default function App() {
   const [index, setIndex] = useState(0);
